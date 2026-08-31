a901161e046eab6b7b7f74d2736720b15d8c1638
Lyra The Fox <122050732+LyraTheFox@users.noreply.github.com>
2026-08-31 15:24:00 -0400

    modified:   package-lock.json 	modified:   package.json 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Sitemap({
+      hostname: 'https://lyrafoxwood.app',
+      dynamicRoutes: [
+        '/',
+        '/about',
+        '/skills',
+        '/music',
+        '/links'
+      ]
+    })
+  ]
 })
